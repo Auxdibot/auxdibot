37b5001dd86b4681c26601dc5ad771f10830d3d0
Steven <58159084+Auxdible@users.noreply.github.com>
2023-07-29 13:24:29 -0400

    feat(record): move record to /punishment & fix record embed

--- src/interaction/subcommands/moderation/punishment/punishmentRecord.ts
+++ src/interaction/subcommands/moderation/punishment/punishmentRecord.ts
@@ -1,25 +1,18 @@
-import { SlashCommandBuilder } from 'discord.js';
-import AuxdibotCommand from '@/interfaces/commands/AuxdibotCommand';
-import AuxdibotCommandInteraction from '@/interfaces/commands/AuxdibotCommandInteraction';
-import { GuildAuxdibotCommandData } from '@/interfaces/commands/AuxdibotCommandData';
 import Modules from '@/constants/bot/commands/Modules';
 import { Auxdibot } from '@/interfaces/Auxdibot';
+import { GuildAuxdibotCommandData } from '@/interfaces/commands/AuxdibotCommandData';
+import AuxdibotCommandInteraction from '@/interfaces/commands/AuxdibotCommandInteraction';
+import { AuxdibotSubcommand } from '@/interfaces/commands/AuxdibotSubcommand';
 import userRecordAsEmbed from '@/modules/features/moderation/userRecordAsEmbed';
 import handleError from '@/util/handleError';
 
-export default <AuxdibotCommand>{
-   data: new SlashCommandBuilder()
-      .setName('record')
-      .setDescription('View a users punishment record.')
-      .addUserOption((builder) =>
-         builder.setName('user').setDescription('The user whose punishments are being displayed.'),
-      ),
+export const punishmentRecord = <AuxdibotSubcommand>{
+   name: 'record',
    info: {
       module: Modules['Moderation'],
-      description:
-         "Displays a user's punishment record. If no user is specified, the user running the command's punishment record.",
-      usageExample: '/record [user]',
-      permission: 'moderation.record',
+      description: "View a user's punishment record.",
+      usageExample: '/punishment record (user)',
+      permission: 'moderation.punishments.record',
    },
    async execute(auxdibot: Auxdibot, interaction: AuxdibotCommandInteraction<GuildAuxdibotCommandData>) {
       if (!interaction.data) return;
