4e6138661fe7a690a79ca1253b36c44ccdd44378
Steven <58159084+Auxdible@users.noreply.github.com>
2023-08-25 20:37:04 -0400

    feat: schedules add endpoint

--- src/server/servers/routes/channels.ts
+++ src/server/servers/routes/channels.ts
@@ -26,6 +26,21 @@ const channels = (auxdibot: Auxdibot, router: Router) => {
          );
       },
    );
+   router.get(
+      '/:serverID/channels/:channelID',
+      (req, res, next) => checkAuthenticated(req, res, next),
+      (req, res) => {
+         if (!req.user?.guilds) return res.status(400).json({ error: 'no servers' });
+         const serverID = req.params.serverID,
+            channelID = req.params.channelID;
+         const guildData = req.user.guilds.find((i) => i.id == serverID);
+         const guild = auxdibot.guilds.cache.get(serverID);
+         if (!guildData || !guild) return res.status(404).json({ error: "couldn't find that server" });
+         if (!guildData.owner && !(guildData.permissions & 0x8))
+            return res.status(403).json({ error: 'you are not authorized to edit that server' });
+         return res.json(guild.channels.cache.get(channelID));
+      },
+   );
    return router;
 };
 export default channels;
--- src/server/servers/routes/schedules.ts
+++ src/server/servers/routes/schedules.ts
@@ -2,61 +2,115 @@ import { Auxdibot } from '@/interfaces/Auxdibot';
 import checkAuthenticated from '@/server/checkAuthenticated';
 import handleLog from '@/util/handleLog';
 import parsePlaceholders from '@/util/parsePlaceholder';
-import { LogAction, ScheduledMessage } from '@prisma/client';
+import timestampToDuration from '@/util/timestampToDuration';
+import { APIEmbed, LogAction, ScheduledMessage } from '@prisma/client';
 import { Router } from 'express';
 /*
    Schedules
    Create, view or delete schedules.
 */
 const schedules = (auxdibot: Auxdibot, router: Router) => {
-   router.route('/:serverID/schedules').get(
-      (req, res, next) => checkAuthenticated(req, res, next),
-      (req, res) => {
-         if (!req.user?.guilds) return res.status(400).json({ error: 'no servers' });
-         const serverID = req.params.serverID;
-         const guildData = req.user.guilds.find((i) => i.id == serverID);
-         const guild = auxdibot.guilds.cache.get(guildData.id);
-         if (!guildData || !guild) return res.status(404).json({ error: "couldn't find that server" });
-         if (!guildData.owner && !(guildData.permissions & 0x8))
-            return res.status(403).json({ error: 'you are not authorized to edit that server' });
+   router
+      .route('/:serverID/schedules')
+      .get(
+         (req, res, next) => checkAuthenticated(req, res, next),
+         (req, res) => {
+            if (!req.user?.guilds) return res.status(400).json({ error: 'no servers' });
+            const serverID = req.params.serverID;
+            const guildData = req.user.guilds.find((i) => i.id == serverID);
+            const guild = auxdibot.guilds.cache.get(guildData.id);
+            if (!guildData || !guild) return res.status(404).json({ error: "couldn't find that server" });
+            if (!guildData.owner && !(guildData.permissions & 0x8))
+               return res.status(403).json({ error: 'you are not authorized to edit that server' });
 
-         return auxdibot.database.servers
-            .findFirst({ where: { serverID: serverID }, select: { serverID: true, scheduled_messages: true } })
-            .then(async (data) =>
-               data
-                  ? res.json({
-                       ...guildData,
-                       data: {
-                          scheduled_messages: await data.scheduled_messages.reduce(
-                             async (
-                                acc:
-                                   | Promise<(ScheduledMessage & { index: number })[]>
-                                   | (ScheduledMessage & { index: number })[],
-                                i,
-                                index,
-                             ) => {
-                                const arr = await acc;
-                                arr.push({
-                                   ...i,
-                                   embed: i.embed
-                                      ? JSON.parse(await parsePlaceholders(auxdibot, JSON.stringify(i.embed), guild))
-                                      : undefined,
+            return auxdibot.database.servers
+               .findFirst({ where: { serverID: serverID }, select: { serverID: true, scheduled_messages: true } })
+               .then(async (data) =>
+                  data
+                     ? res.json({
+                          ...guildData,
+                          data: {
+                             scheduled_messages: await data.scheduled_messages.reduce(
+                                async (
+                                   acc:
+                                      | Promise<(ScheduledMessage & { index: number })[]>
+                                      | (ScheduledMessage & { index: number })[],
+                                   i,
                                    index,
-                                });
-                                return arr;
-                             },
-                             [] as (ScheduledMessage & { index: number })[],
-                          ),
-                       },
-                    })
-                  : res.status(404).json({ error: "couldn't find that server" }),
-            )
-            .catch((x) => {
-               console.error(x);
+                                ) => {
+                                   const arr = await acc;
+                                   arr.push({
+                                      ...i,
+                                      embed: i.embed
+                                         ? {
+                                              ...JSON.parse(
+                                                 await parsePlaceholders(auxdibot, JSON.stringify(i.embed), guild),
+                                              ),
+                                              color: '#' + i.embed.color?.toString(16),
+                                           }
+                                         : undefined,
+                                      index,
+                                   });
+                                   return arr;
+                                },
+                                [] as (ScheduledMessage & { index: number })[],
+                             ),
+                          },
+                       })
+                     : res.status(404).json({ error: "couldn't find that server" }),
+               )
+               .catch((x) => {
+                  console.error(x);
+                  return res.status(500).json({ error: 'an error occurred' });
+               });
+         },
+      )
+      .post(
+         (req, res, next) => checkAuthenticated(req, res, next),
+         async (req, res) => {
+            if (!req.user?.guilds) return res.status(400).json({ error: 'no servers' });
+            if (!req.body['duration'] || !req.body['channel'] || (!req.body['message'] && !req.body['embed']))
+               return res.status(400).json({ error: 'missing parameters' });
+            const serverID = req.params.serverID;
+            const guildData = req.user.guilds.find((i) => i.id == serverID);
+            const guild = auxdibot.guilds.cache.get(guildData.id);
+            if (!guildData || !guild) return res.status(404).json({ error: "couldn't find that server" });
+            if (!guildData.owner && !(guildData.permissions & 0x8))
+               return res.status(403).json({ error: 'you are not authorized to edit that server' });
+            try {
+               const duration = timestampToDuration(req.body['duration']);
+               const channel = guild.channels.cache.get(req.body['channel']);
+               const embed = req.body['embed'] ? (JSON.parse(req.body['embed']) satisfies APIEmbed) : undefined;
+               if (!duration || duration == 'permanent') return res.status(400).json({ error: 'invalid duration' });
+               if (!channel) return res.status(400).json({ error: 'invalid channel' });
+               const scheduledMessage = {
+                  interval_unix: duration,
+                  message: req.body['message'],
+                  embed: embed
+                     ? { ...embed, color: embed.color ? parseInt(embed.color.replace('#', ''), 16) : null }
+                     : undefined,
+                  last_run_unix: Date.now() - duration,
+                  times_to_run: req.body['times_to_run'] ? Number(req.body['times_to_run']) : undefined,
+                  times_run: 0,
+                  channelID: req.body['channel'],
+               } satisfies ScheduledMessage;
+               await auxdibot.database.servers.update({
+                  where: { serverID: guildData.id },
+                  data: { scheduled_messages: { push: scheduledMessage } },
+               });
+               await handleLog(auxdibot, guild, {
+                  userID: req.user.id,
+                  description: `Scheduled a message for ${channel.name}.`,
+                  type: LogAction.SCHEDULED_MESSAGE_CREATED,
+                  date_unix: Date.now(),
+               });
+               return res.json(scheduledMessage);
+            } catch (x) {
+               console.log(x);
                return res.status(500).json({ error: 'an error occurred' });
-            });
-      },
-   );
+            }
+         },
+      );
    router.route('/:serverID/schedules/:id').delete(
       (req, res, next) => checkAuthenticated(req, res, next),
       (req, res) => {
