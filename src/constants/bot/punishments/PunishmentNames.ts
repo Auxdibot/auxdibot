ada3828ae5764b345b505388e8bdda446c89d5fb
Steven <58159084+Auxdible@users.noreply.github.com>
2023-11-10 12:04:18 -0500

    feat(constants): update constants for automod

--- src/constants/bot/punishments/PunishmentNames.ts
+++ src/constants/bot/punishments/PunishmentNames.ts
@@ -1,4 +1,10 @@
-export const PunishmentNames = {
+import { PunishmentType } from '@prisma/client';
+
+export const PunishmentNames: { [k in PunishmentType]: { name: string; action: string } } = {
+   DELETE_MESSAGE: {
+      name: '🗑️ Message Deleted',
+      action: 'message deleted',
+   },
    WARN: {
       name: '⚠ Warn',
       action: 'warned',
