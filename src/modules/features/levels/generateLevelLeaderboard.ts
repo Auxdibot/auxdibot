c2b7ba2e287de42cb34928a0efb5384c721c02d6
Steven <58159084+Auxdible@users.noreply.github.com>
2023-12-12 16:51:28 -0500

    fix: leaderboard & feat: leaderboard improvements

--- src/modules/features/levels/generateLevelLeaderboard.ts
+++ src/modules/features/levels/generateLevelLeaderboard.ts
@@ -1,13 +1,14 @@
 import { Auxdibot } from '@/interfaces/Auxdibot';
 import { servermembers } from '@prisma/client';
-import { Collection, LimitedCollection } from 'discord.js';
 
 export default async function generateLevelLeaderboard(auxdibot: Auxdibot, serverID: string, limit?: number) {
    const members = await auxdibot.database.servermembers.findMany({ where: { serverID } });
-   const leaderboard = members
-      .reduce((acc: Collection<servermembers, number>, member: servermembers) => {
-         return acc.set(member, member.xp);
-      }, new Collection())
-      .sort((a, b) => b - a);
-   return limit ? new LimitedCollection({ maxSize: limit }, leaderboard) : leaderboard;
+   let leaderboard = members
+      .reduce((acc: servermembers[], member: servermembers) => {
+         acc.push(member);
+         return acc;
+      }, [])
+      .sort((a, b) => b.xp - a.xp);
+   if (limit) leaderboard = leaderboard.slice(0, limit ? limit : undefined);
+   return leaderboard;
 }
