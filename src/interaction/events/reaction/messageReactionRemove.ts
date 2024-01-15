ed940f6f9a195dd6b833b5ce1409a5ed48f8b164
Steven <58159084+Auxdible@users.noreply.github.com>
2024-01-15 14:43:18 -0500

    feat: sticky reaction roles

--- src/interaction/events/reaction/messageReactionRemove.ts
+++ src/interaction/events/reaction/messageReactionRemove.ts
@@ -1,4 +1,4 @@
-import { MessageReaction, PartialMessageReaction } from 'discord.js';
+import { GuildMember, MessageReaction, PartialMessageReaction, PartialUser, User } from 'discord.js';
 import Modules from '@/constants/bot/commands/Modules';
 import { Auxdibot } from '@/interfaces/Auxdibot';
 import findOrCreateServer from '@/modules/server/findOrCreateServer';
@@ -8,8 +8,22 @@ import deleteStarredMessage from '@/modules/features/starboard/deleteStarredMess
 export default async function messageReactionRemove(
    auxdibot: Auxdibot,
    messageReaction: MessageReaction | PartialMessageReaction,
+   user: User | PartialUser,
 ) {
    const server = await findOrCreateServer(auxdibot, messageReaction.message.guildId);
+   const member: GuildMember | null = messageReaction.message.guild.members.resolve(user.id);
+   if (!member) return;
+   if (!server.disabled_modules.find((item) => item == Modules['Roles'].name)) {
+      const rrData = server.reaction_roles.find((rr) => messageReaction.message.id == rr.messageID);
+      if (rrData) {
+         const rr = rrData.reactions.find(
+            (react) => react.emoji == (messageReaction.emoji.valueOf() || messageReaction.emoji.toString()),
+         );
+         if (rr) {
+            if (member.roles.resolve(rr.role)) await member.roles.remove(rr.role).catch(() => undefined);
+         }
+      }
+   }
    if (!server.disabled_modules.find((item) => item == Modules['Starboard'].name)) {
       const starred = server.starred_messages.find((i) => i.starred_message_id == messageReaction.message.id);
       const starboard_channel = messageReaction.message.guild.channels.cache.get(server.starboard_channel);
