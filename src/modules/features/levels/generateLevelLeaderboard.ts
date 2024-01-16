8fe8fc2bb7ab1e62c3a1a05a0236561261dff37f
Steven <58159084+Auxdible@users.noreply.github.com>
2024-01-16 14:02:14 -0500

    fix: members not in server shouldn't be on leaderboard

--- src/modules/features/levels/generateLevelLeaderboard.ts
+++ src/modules/features/levels/generateLevelLeaderboard.ts
@@ -2,7 +2,7 @@ import { Auxdibot } from '@/interfaces/Auxdibot';
 import { servermembers } from '@prisma/client';
 
 export default async function generateLevelLeaderboard(auxdibot: Auxdibot, serverID: string, limit?: number) {
-   const members = await auxdibot.database.servermembers.findMany({ where: { serverID } });
+   const members = await auxdibot.database.servermembers.findMany({ where: { serverID, in_server: true } });
    let leaderboard = members
       .reduce((acc: servermembers[], member: servermembers) => {
          acc.push(member);
