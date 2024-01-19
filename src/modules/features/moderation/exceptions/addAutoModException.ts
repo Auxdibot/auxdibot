42051b78a58b9d8cccbf849a2ccbc58d6f1dc96f
Steven <58159084+Auxdible@users.noreply.github.com>
2024-01-19 11:31:58 -0500

    feat: deprecate mute role, implement autoMod exception limit and mod threshold

--- src/modules/features/moderation/exceptions/addAutoModException.ts
+++ src/modules/features/moderation/exceptions/addAutoModException.ts
@@ -1,9 +1,15 @@
+import Limits from '@/constants/database/Limits';
 import { Auxdibot } from '@/interfaces/Auxdibot';
+import findOrCreateServer from '@/modules/server/findOrCreateServer';
 import handleLog from '@/util/handleLog';
+import { testLimit } from '@/util/testLimit';
 import { LogAction } from '@prisma/client';
 import { APIRole, Guild, Role } from 'discord.js';
 
 export async function addAutoModException(auxdibot: Auxdibot, guild: Guild, role: Role | APIRole, userID?: string) {
+   const server = await findOrCreateServer(auxdibot, guild.id);
+   if (!testLimit(server.automod_role_exceptions, Limits.AUTOMOD_EXCEPTION_LIMIT))
+      throw new Error('You have too many role exceptions!');
    return await auxdibot.database.servers
       .update({
          where: { serverID: guild.id },
