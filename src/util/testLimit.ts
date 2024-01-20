451f323fd38356e67614891064be4c7924af700d
Steven <58159084+Auxdible@users.noreply.github.com>
2024-01-20 15:48:27 -0500

    fix: bug with permissions on message delete & moderation endpoints

--- src/util/testLimit.ts
+++ src/util/testLimit.ts
@@ -1,5 +1,5 @@
 export const testLimit = (v: any[], limit: number, purgePrior?: boolean) => {
-   if (v.length <= limit) return true;
+   if (v.length < limit) return true;
    if (purgePrior) {
       v.splice(0, v.length - limit);
       return 'spliced';
