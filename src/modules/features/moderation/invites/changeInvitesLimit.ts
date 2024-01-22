4009e8bc09f930bc1f89ebf8e8af870f46cafaf9
Steven <58159084+Auxdible@users.noreply.github.com>
2024-01-22 16:12:19 -0500

    feat: create feature funcs for automod limits

---
+++ src/modules/features/moderation/invites/changeInvitesLimit.ts
@@ -0,0 +1,30 @@
+import { Auxdibot } from '@/interfaces/Auxdibot';
+import handleLog from '@/util/handleLog';
+import { AutomodLimit, LogAction } from '@prisma/client';
+import { Guild } from 'discord.js';
+
+export default async function changeInvitesLimit(
+   auxdibot: Auxdibot,
+   guild: Guild,
+   user: { id: string; username: string },
+   invites: AutomodLimit | null,
+) {
+   return auxdibot.database.servers
+      .update({
+         where: { serverID: guild.id },
+         data: { automod_invites_limit: invites },
+         select: { automod_invites_limit: true },
+      })
+      .then(async (i) => {
+         await handleLog(auxdibot, guild, {
+            userID: user.id,
+            description:
+               invites.messages == 0 || invites.duration == 0
+                  ? 'Disabled invites filter.'
+                  : `The Automod invites limit has been set to ${invites.messages} invites every ${invites.duration} seconds.`,
+            type: LogAction.AUTOMOD_SETTINGS_CHANGE,
+            date_unix: Date.now(),
+         });
+         return i;
+      });
+}
---
+++ src/modules/features/moderation/spam/changeSpamLimit.ts
@@ -0,0 +1,30 @@
+import { Auxdibot } from '@/interfaces/Auxdibot';
+import handleLog from '@/util/handleLog';
+import { AutomodLimit, LogAction } from '@prisma/client';
+import { Guild } from 'discord.js';
+
+export default async function changeSpamLimit(
+   auxdibot: Auxdibot,
+   guild: Guild,
+   user: { id: string; username: string },
+   spam: AutomodLimit | null,
+) {
+   return auxdibot.database.servers
+      .update({
+         where: { serverID: guild.id },
+         data: { automod_spam_limit: spam },
+         select: { automod_spam_limit: true },
+      })
+      .then(async (i) => {
+         await handleLog(auxdibot, guild, {
+            userID: user.id,
+            description:
+               spam.messages == 0 || spam.duration == 0
+                  ? 'Disabled spam filter.'
+                  : `The Automod spam limit has been set to ${spam.messages} messages every ${spam.duration} seconds.`,
+            type: LogAction.AUTOMOD_SETTINGS_CHANGE,
+            date_unix: Date.now(),
+         });
+         return i;
+      });
+}
