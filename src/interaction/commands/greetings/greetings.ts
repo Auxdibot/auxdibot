fee6f33bade78501f882af1bdf49b20e5e748e3e
Steven <58159084+Auxdible@users.noreply.github.com>
2024-01-30 17:26:10 -0500

    feat: move joinLeaveChannel to greetings channel and setup for new changes

---
+++ src/interaction/commands/greetings/greetings.ts
@@ -0,0 +1,31 @@
+import { ChannelType, SlashCommandBuilder } from 'discord.js';
+import AuxdibotCommand from '@/interfaces/commands/AuxdibotCommand';
+import Modules from '@/constants/bot/commands/Modules';
+import { greetingsChannel } from '@/interaction/subcommands/greetings/greetingsChannel';
+
+export default <AuxdibotCommand>{
+   data: new SlashCommandBuilder()
+      .setName('greetings')
+      .setDescription('Change settings for greetings on the server.')
+      .addSubcommand((builder) =>
+         builder
+            .setName('channel')
+            .setDescription('Set the greetings channel for this server, where join and leave messages are broadcast.')
+            .addChannelOption((builder) =>
+               builder
+                  .setName('channel')
+                  .setDescription('The channel to broadcast join and leave messages to.')
+                  .addChannelTypes(ChannelType.GuildText),
+            ),
+      ),
+   info: {
+      module: Modules['Greetings'],
+      description: 'Change settings for greetings on the server.',
+      usageExample: '/greetings (channel)',
+      permission: 'greetings',
+   },
+   subcommands: [greetingsChannel],
+   async execute() {
+      return;
+   },
+};
