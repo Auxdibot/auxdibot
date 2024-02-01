d11204e8c610e23f582e19eb65665b140eed05ff
Steven <58159084+Auxdible@users.noreply.github.com>
2024-02-01 09:02:04 -0500

    feat: add reaction roles add message, fix: deletion issues, fix: descriptions for subcomms

--- src/interaction/subcommands/roles/reactionRoles/reactionRolesAddMessage.ts
+++ src/interaction/subcommands/roles/reactionRoles/reactionRolesAddMessage.ts
@@ -4,29 +4,30 @@ import { Auxdibot } from '@/interfaces/Auxdibot';
 import { GuildAuxdibotCommandData } from '@/interfaces/commands/AuxdibotCommandData';
 import AuxdibotCommandInteraction from '@/interfaces/commands/AuxdibotCommandInteraction';
 import { AuxdibotSubcommand } from '@/interfaces/commands/AuxdibotSubcommand';
-import addReactionRole from '@/modules/features/roles/reaction_roles/addReactionRole';
+import { applyReactionsToMessages } from '@/modules/features/roles/reaction_roles/applyReactionsToMessage';
+import { getMessage } from '@/util/getMessage';
 import handleError from '@/util/handleError';
-import parsePlaceholders from '@/util/parsePlaceholder';
+import handleLog from '@/util/handleLog';
 import { testLimit } from '@/util/testLimit';
 import { EmbedBuilder } from '@discordjs/builders';
-import { APIEmbed, ReactionRoleType } from '@prisma/client';
-import { ChannelType } from 'discord.js';
+import { LogAction, ReactionRoleType } from '@prisma/client';
 
-export const reactionRolesAddJSON = <AuxdibotSubcommand>{
-   name: 'add_json',
+export const reactionRolesAddMessage = <AuxdibotSubcommand>{
+   name: 'add_message',
    info: {
       module: Modules['Roles'],
-      description: 'Add a reaction role to the server with custom Discord Embed JSON.',
-      usageExample: '/reaction_roles add_json (channel) (roles) [type] (json)',
-      permission: 'rr.add.json',
+      description: 'Add a reaction role to the server using an existing message.',
+      usageExample: '/reaction_roles add_message (messageID) (roles) [type]',
+      permission: 'rr.add.message',
    },
    async execute(auxdibot: Auxdibot, interaction: AuxdibotCommandInteraction<GuildAuxdibotCommandData>) {
       if (!interaction.data) return;
-      const channel = interaction.options.getChannel('channel', true, [ChannelType.GuildText]),
+      const messageID = interaction.options.getString('message', true),
          roles = interaction.options.getString('roles', true),
-         json = interaction.options.getString('json', true),
          type = interaction.options.getString('type', false) || 'DEFAULT';
-
+      const split = roles.split(' ');
+      const builder = [];
+      const server = interaction.data.guildData;
       if (!testLimit(interaction.data.guildData.reaction_roles, Limits.REACTION_ROLE_DEFAULT_LIMIT)) {
          return await handleError(
             auxdibot,
@@ -35,12 +36,12 @@ export const reactionRolesAddJSON = <AuxdibotSubcommand>{
             interaction,
          );
       }
-      if (!ReactionRoleType[type]) {
+      if (
+         !ReactionRoleType[type] ||
+         !['DEFAULT', 'SELECT_ONE', 'STICKY', 'STICKY_SELECT_ONE'].includes(ReactionRoleType[type])
+      ) {
          return await handleError(auxdibot, 'INVALID_TYPE', 'This is not a valid reaction role type!', interaction);
       }
-
-      const split = roles.split(' ');
-      const builder = [];
       while (split.length) {
          const [emoji, roleID] = split.splice(0, 2);
          builder.push({ emoji, roleID });
@@ -53,32 +54,30 @@ export const reactionRolesAddJSON = <AuxdibotSubcommand>{
             interaction,
          );
       }
-      const embed = JSON.parse(
-         await parsePlaceholders(auxdibot, json || '', interaction.data.guild, interaction.data.member),
-      ) satisfies APIEmbed;
-      addReactionRole(
-         auxdibot,
-         interaction.guild,
-         channel,
-         embed.title,
-         builder,
-         embed,
-         undefined,
-         ReactionRoleType[type],
-      )
+      const message = await getMessage(interaction.guild, messageID);
+      if (!message || server.reaction_roles.find((i) => i.messageID == message.id)) {
+         return await handleError(auxdibot, 'INVALID_MESSAGE', 'This is not a valid message!', interaction);
+      }
+      applyReactionsToMessages(auxdibot, interaction.guild, message, builder, ReactionRoleType[type])
          .then(async () => {
             const resEmbed = new EmbedBuilder().setColor(auxdibot.colors.accept).toJSON();
-            resEmbed.title = '👈 Created Reaction Role';
-            resEmbed.description = `Created a reaction role in ${channel}`;
+            resEmbed.title = '👈 Applied Reaction Role';
+            resEmbed.description = `Applied your reaction role to your message in ${message.channel}!`;
+            handleLog(auxdibot, interaction.data.guild, {
+               userID: interaction.data.member.id,
+               description: `Applied a reaction role to a message in ${message.channel}`,
+               type: LogAction.REACTION_ROLE_ADDED,
+               date_unix: Date.now(),
+            });
             return await interaction.reply({ embeds: [resEmbed] });
          })
-         .catch(async (x) => {
+         .catch((x) =>
             handleError(
                auxdibot,
                'REACTION_ROLE_CREATE_ERROR',
-               typeof x.message == 'string' ? x.message : "Couldn't delete that permission override!",
+               typeof x.message == 'string' ? x.message : "Couldn't create that reaction role!",
                interaction,
-            );
-         });
+            ),
+         );
    },
 };
---
+++ src/modules/features/roles/reaction_roles/applyReactionRoles.ts
@@ -0,0 +1,68 @@
+import { ReactionsAndRolesBuilder } from '@/interfaces/reactions/ReactionsAndRolesBuilder';
+import { ReactionRoleType } from '@prisma/client';
+import {
+   ActionRowBuilder,
+   ButtonBuilder,
+   ButtonStyle,
+   Message,
+   StringSelectMenuBuilder,
+   StringSelectMenuOptionBuilder,
+} from 'discord.js';
+
+export default async function applyReactionRoles(
+   msg: Message,
+   reactionsAndRoles: ReactionsAndRolesBuilder[],
+   type: ReactionRoleType,
+) {
+   let rows: ActionRowBuilder<ButtonBuilder | StringSelectMenuBuilder>[] = [
+      new ActionRowBuilder<StringSelectMenuBuilder>(),
+   ];
+   switch (type) {
+      case ReactionRoleType.DEFAULT:
+      case ReactionRoleType.STICKY_SELECT_ONE:
+      case ReactionRoleType.STICKY:
+      case ReactionRoleType.SELECT_ONE:
+         reactionsAndRoles.forEach((i) => msg.react(i.emoji).catch(() => undefined));
+         break;
+      case ReactionRoleType.BUTTON:
+      case ReactionRoleType.BUTTON_SELECT_ONE:
+         rows = Array.from({ length: Math.ceil(reactionsAndRoles.length / 5) }, (v, i) =>
+            reactionsAndRoles.slice(i * 5, i * 5 + 5),
+         ).map((a) =>
+            new ActionRowBuilder<ButtonBuilder>().addComponents(
+               a.map((i) =>
+                  new ButtonBuilder()
+                     .setCustomId(`rr-${i.role.id}`)
+                     .setEmoji(i.emoji)
+                     .setLabel(i.role.name)
+                     .setStyle(ButtonStyle.Secondary),
+               ),
+            ),
+         );
+         break;
+      case ReactionRoleType.SELECT_MENU:
+      case ReactionRoleType.SELECT_ONE_MENU:
+         rows[0].addComponents(
+            new StringSelectMenuBuilder()
+               .setCustomId('rr')
+               .setMinValues(1)
+               .setMaxValues(type == 'SELECT_ONE_MENU' ? 1 : reactionsAndRoles.length)
+               .setPlaceholder('Select a role.')
+               .setDisabled(false)
+               .addOptions(
+                  ...reactionsAndRoles.map((i) =>
+                     new StringSelectMenuOptionBuilder()
+                        .setEmoji(i.emoji)
+                        .setLabel(i.role.name)
+                        .setValue(i.role.id)
+                        .setDescription(`You will receive the ${i.role.name} role.`),
+                  ),
+               ),
+         );
+   }
+   if (rows.find((i) => i.components.length > 0)) {
+      await msg.edit({
+         components: rows.map((i) => i.toJSON()),
+      });
+   }
+}
