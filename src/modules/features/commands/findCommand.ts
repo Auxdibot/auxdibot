282449696e5519b0093fd7211ae7c5955fc41631
Steven <58159084+Auxdible@users.noreply.github.com>
2024-03-20 23:00:04 -0400

    feat: implement settings set for admin only

---
+++ src/modules/features/commands/findCommand.ts
@@ -0,0 +1,18 @@
+import { Auxdibot } from '@/interfaces/Auxdibot';
+import AuxdibotCommand from '@/interfaces/commands/AuxdibotCommand';
+import { AuxdibotSubcommand } from '@/interfaces/commands/AuxdibotSubcommand';
+
+export function findCommand(
+   auxdibot: Auxdibot,
+   command: string,
+   subcommand?: string[],
+): { commandData: AuxdibotCommand; subcommandData?: AuxdibotSubcommand } | undefined {
+   const commandData = auxdibot.commands.get(command),
+      subcommandData = commandData?.subcommands.find((i) =>
+         subcommand.length > 1 ? i.name == subcommand[0] && i.group == subcommand[1] : i.name == subcommand[0],
+      );
+   if (!commandData || (subcommand.length && !subcommandData)) {
+      return undefined;
+   }
+   return { commandData, subcommandData };
+}
