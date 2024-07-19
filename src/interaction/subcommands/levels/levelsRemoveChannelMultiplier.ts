d71e73dd6724bac27fb8a30f7d5873d4af3dfd9a
Steven <58159084+Auxdible@users.noreply.github.com>
2024-07-19 13:13:11 -0400

    feat(levels): add in channel multipliers

---
+++ src/interaction/subcommands/levels/levelsRemoveChannelMultiplier.ts
@@ -0,0 +1,68 @@
+import Modules from '@/constants/bot/commands/Modules';
+import { Auxdibot } from '@/interfaces/Auxdibot';
+import { GuildAuxdibotCommandData } from '@/interfaces/commands/AuxdibotCommandData';
+import AuxdibotCommandInteraction from '@/interfaces/commands/AuxdibotCommandInteraction';
+import { AuxdibotSubcommand } from '@/interfaces/commands/AuxdibotSubcommand';
+import deleteChannelMultiplier from '@/modules/features/levels/deleteChannelMultiplier';
+import handleError from '@/util/handleError';
+import { EmbedBuilder } from '@discordjs/builders';
+import { ChannelType } from 'discord.js';
+
+export const levelsRemoveChannelMultiplier = <AuxdibotSubcommand>{
+   name: 'remove_channel',
+   group: 'multipliers',
+   info: {
+      module: Modules['Levels'],
+      description: 'Remove a reward from the Level Rewards.',
+      usageExample: '/levels multipliers remove_channel (channel|index)',
+   },
+   async execute(auxdibot: Auxdibot, interaction: AuxdibotCommandInteraction<GuildAuxdibotCommandData>) {
+      if (!interaction.data) return;
+
+      const server = interaction.data.guildData,
+         channel = interaction.options.getChannel('channel', false, [
+            ChannelType.GuildAnnouncement,
+            ChannelType.GuildText,
+            ChannelType.GuildVoice,
+            ChannelType.GuildStageVoice,
+         ]),
+         index = interaction.options.getInteger('index', false);
+      if (!channel && !index) {
+         return await handleError(
+            auxdibot,
+            'CHANNEL_INVALID',
+            'You must provide a valid channel or index.',
+            interaction,
+         );
+      }
+      const multiplier = server.channel_multipliers.find((reward, i) => reward.id == channel.id || i == index);
+      if (!multiplier) {
+         return await handleError(
+            auxdibot,
+            'CHANNEL_MULTIPLIER_NOT_FOUND',
+            'The channel multiplier specified does not exist!',
+            interaction,
+         );
+      }
+      deleteChannelMultiplier(
+         auxdibot,
+         interaction.guild,
+         interaction.user,
+         server.channel_multipliers.indexOf(multiplier),
+      )
+         .then(async () => {
+            const embed = new EmbedBuilder().setColor(auxdibot.colors.accept).toJSON();
+            embed.title = 'Channel Multiplier Removed';
+            embed.description = `Successfully removed the channel multiplier for <#${multiplier.id}>.`;
+            return await auxdibot.createReply(interaction, { embeds: [embed] });
+         })
+         .catch((x) => {
+            handleError(
+               auxdibot,
+               'CHANNEL_MULTIPLIER_REMOVE_ERROR',
+               typeof x.message == 'string' ? x.message : "Couldn't remove that channel multiplier.",
+               interaction,
+            );
+         });
+   },
+};
---
+++ src/modules/features/levels/deleteChannelMultiplier.ts
@@ -0,0 +1,32 @@
+import { Auxdibot } from '@/interfaces/Auxdibot';
+import findOrCreateServer from '@/modules/server/findOrCreateServer';
+import handleLog from '@/util/handleLog';
+import { LogAction } from '@prisma/client';
+import { Guild } from 'discord.js';
+
+export default async function deleteChannelMultiplier(
+   auxdibot: Auxdibot,
+   guild: Guild,
+   user: { id: string },
+   id: number,
+) {
+   const server = await findOrCreateServer(auxdibot, guild.id);
+   if (server.channel_multipliers.length <= id) throw new Error('Invalid id provided.');
+
+   const multiplier = server.channel_multipliers.splice(id, 1);
+   return await auxdibot.database.servers
+      .update({
+         where: { serverID: guild.id },
+         data: { channel_multipliers: server.channel_multipliers },
+         select: { channel_multipliers: true },
+      })
+      .then((data) => {
+         handleLog(auxdibot, guild, {
+            type: LogAction.MULTIPLIER_DELETED,
+            userID: user.id,
+            date_unix: Date.now(),
+            description: `Deleted the channel multiplier #${id} (in <#${multiplier[0]?.id}>) from your server.`,
+         });
+         return data;
+      });
+}
