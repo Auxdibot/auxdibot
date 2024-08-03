c24451ba161738b8c712e212feb67bbb1b52e83b
Steven Primeaux <stevenhotrod06@gmail.com>
2024-08-03 15:48:29 +0000

    fix(embed): fix embed empty function to keep some things from crashing

--- src/util/isEmbedEmpty.ts
+++ src/util/isEmbedEmpty.ts
@@ -1,8 +1,9 @@
 import { APIEmbed } from '@prisma/client';
 
-export const isEmbedEmpty = (embed: APIEmbed) =>
-   !embed.author?.name &&
-   !embed.title &&
-   !embed.description &&
-   (!embed.fields || embed.fields.length == 0) &&
-   !embed.footer?.text;
+export const isEmbedEmpty = (embed?: APIEmbed) =>
+   !embed ||
+   (!embed.author?.name &&
+      !embed.title &&
+      !embed.description &&
+      (!embed.fields || embed.fields.length == 0) &&
+      !embed.footer?.text);
