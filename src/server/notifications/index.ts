6f08b4a6ee4f2409ca6dcedad869c0c57a8d9d53
Steven <58159084+Auxdible@users.noreply.github.com>
2024-08-28 08:09:13 -0400

    feat(notifications): fix bad logic for twitch notifications, change env var SITE_URL to API_URL

--- src/server/notifications/index.ts
+++ src/server/notifications/index.ts
@@ -17,56 +17,60 @@ const router = express.Router();
 
 export const notificationsRoute = (auxdibot: Auxdibot) => {
    router.use(express.raw({ type: 'application/json' })).post('/callbacks/twitch', async (req, res) => {
-      const notification = JSON.parse(req.body);
-      const message = getTwitchHmacMessage(req);
-      const hmac = `sha256=${getTwitchHmac(process.env.HMAC_SECRET, message)}`;
-      if (!verifyTwitchMessage(hmac, req.headers[TWITCH_MESSAGE_SIGNATURE])) return res.status(403);
-      if (req.headers[TWITCH_MESSAGE_TYPE] == MESSAGE_TYPE_VERIFICATION)
-         return res.status(200).send(notification.challenge);
-      if (req.headers[TWITCH_MESSAGE_TYPE] === 'revocation') return res.status(204);
-      if (req.headers[TWITCH_MESSAGE_TYPE] !== 'notification') return res.status(400);
-      // Handle notification
-      const event = notification['event'];
-      if (!event) return res.status(204);
-      const author = event['broadcaster_user_login'],
-         link = `https://twitch.tv/${author}`;
-      const data = <GenericFeed>{
-         author: author,
-         link: link,
-         date: new Date(event['started_at']).valueOf(),
-         title: event['id'],
-      };
-      const servers = await auxdibot.database.servers.findMany({}).catch(() => []);
-      for (const server of servers) {
-         for (const i of server.notifications) {
-            if (i.topicURL == author) {
-               if (
-                  data &&
-                  JSON.stringify(data) != i.previous_data &&
-                  (data?.date ?? Date.now()) > (JSON.parse(i.previous_data)?.date ?? 0)
-               ) {
-                  const guild = auxdibot.guilds.cache.get(server.serverID);
-                  server.notifications[server.notifications.indexOf(i)] = {
-                     ...i,
-                     previous_data: JSON.stringify(data),
-                  };
-                  auxdibot.database.servers
-                     .update({
-                        where: { serverID: guild.id },
-                        data: { notifications: server.notifications },
-                     })
-                     .then(async () => {
-                        publishNotification(auxdibot, guild, i, data).catch(() => undefined);
-                     })
-                     .catch(() => {
-                        return undefined;
-                     });
+      try {
+         const notification = JSON.parse(req.body);
+         const message = getTwitchHmacMessage(req);
+         const hmac = `sha256=${getTwitchHmac(process.env.HMAC_SECRET, message)}`;
+         if (!verifyTwitchMessage(hmac, req.headers[TWITCH_MESSAGE_SIGNATURE])) return res.status(403);
+         if (req.headers[TWITCH_MESSAGE_TYPE] == MESSAGE_TYPE_VERIFICATION)
+            return res.status(200).send(notification.challenge);
+         if (req.headers[TWITCH_MESSAGE_TYPE] === 'revocation') return res.status(204);
+         if (req.headers[TWITCH_MESSAGE_TYPE] !== 'notification') return res.status(400);
+         // Handle notification
+         const event = notification['event'];
+         if (!event) return res.status(204);
+         const author = event['broadcaster_user_login'],
+            link = `https://twitch.tv/${author}`;
+         const data = <GenericFeed>{
+            author: author,
+            link: link,
+            date: new Date(event['started_at']).valueOf(),
+            title: event['id'],
+         };
+         const servers = await auxdibot.database.servers.findMany({}).catch(() => []);
+         for (const server of servers) {
+            for (const i of server.notifications) {
+               if (i.topicURL == author) {
+                  if (
+                     data &&
+                     JSON.stringify(data) != i.previous_data &&
+                     (data?.date ?? Date.now()) > (JSON.parse(i.previous_data)?.date ?? 0)
+                  ) {
+                     const guild = auxdibot.guilds.cache.get(server.serverID);
+                     server.notifications[server.notifications.indexOf(i)] = {
+                        ...i,
+                        previous_data: JSON.stringify(data),
+                     };
+                     auxdibot.database.servers
+                        .update({
+                           where: { serverID: guild.id },
+                           data: { notifications: server.notifications },
+                        })
+                        .then(async () => {
+                           publishNotification(auxdibot, guild, i, data).catch(() => undefined);
+                        })
+                        .catch((x) => {
+                           return undefined;
+                        });
+                  }
                }
             }
          }
-      }
 
-      res.status(200);
+         res.status(200);
+      } catch (x) {
+         res.status(500);
+      }
    });
    return router;
 };
