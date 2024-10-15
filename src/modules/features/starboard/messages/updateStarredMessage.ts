1ff4917eb0791cbaac65aa7b5cf29fc13832aff6
Steven <58159084+Auxdible@users.noreply.github.com>
2024-10-15 12:11:57 -0400

    feat(starboard): improve starboard handling

--- src/modules/features/starboard/messages/updateStarredMessage.ts
+++ src/modules/features/starboard/messages/updateStarredMessage.ts
@@ -5,6 +5,7 @@ import { StarboardBoardData, StarredMessage } from '@prisma/client';
 import { ActionRowBuilder, ButtonBuilder, ButtonStyle, EmbedBuilder, Guild, GuildBasedChannel } from 'discord.js';
 import { defaultStarLevels } from '@/constants/database/defaultStarLevels';
 import { getMessage } from '@/util/getMessage';
+import deleteStarredMessage from './deleteStarredMessage';
 export default async function updateStarredMessage(
    auxdibot: Auxdibot,
    guild: Guild,
@@ -12,18 +13,37 @@ export default async function updateStarredMessage(
    starredData: StarredMessage,
    count: number,
 ) {
-   const starredMessage = await getMessage(guild, starredData.starred_message_id).catch(() => undefined);
+   // Fetch starred channel from context
+   const channel = starredData.starred_channel_id
+      ? await guild.channels.fetch(starredData.starred_channel_id)
+      : undefined;
+   if ((starredData.starred_channel_id && !channel) || !channel.isTextBased()) return;
+
+   // Fetch starboard channel from context
+   const board_channel = guild.channels.cache.get(board.channelID);
+   if (!board_channel || !board_channel.isTextBased()) return;
+
+   // Fetch starred message and starboard message, if applicable.
+   const starredMessage = starredData.starred_channel_id
+      ? await channel.messages.fetch(starredData.starred_message_id)
+      : await getMessage(guild, starredData.starred_message_id);
    if (!starredMessage) return;
+
+   // Fetch starboard channel
    const starboard_channel: GuildBasedChannel | undefined = await guild.channels
       .fetch(board.channelID)
       .catch(() => undefined);
    if (!starboard_channel || !starboard_channel.isTextBased()) return;
+
+   // Fetch starboard message
    const message = await starboard_channel.messages.fetch(starredData.starboard_message_id).catch(() => undefined);
 
+   // Get starboard level to use for starboard message
    const starLevelsSorted = board.star_levels.sort((a, b) => b.stars - a.stars);
    const starLevel = starLevelsSorted.find((i) => count >= board.count * i.stars) ??
       starLevelsSorted[0] ?? { ...defaultStarLevels[defaultStarLevels.length - 1], message_reaction: board.reaction };
 
+   // Update starboard message
    if (message) {
       try {
          const jsonEmbed = structuredClone(DEFAULT_STARBOARD_MESSAGE_EMBED);
@@ -35,13 +55,17 @@ export default async function updateStarredMessage(
                starred_data: starredData,
             }),
          );
+
+         // Handle references, quotes, attachments.
          const reference = await starredMessage.fetchReference().catch(() => undefined);
+
          const quoteEmbed = reference
             ? new EmbedBuilder()
                  .setTitle('Reply to')
                  .setAuthor({ name: reference.author.tag, iconURL: reference.author.avatarURL({ size: 128 }) })
                  .setDescription(reference.cleanContent)
             : null;
+
          const attachmentsComponent = new ActionRowBuilder<ButtonBuilder>();
          starredMessage.attachments.forEach((i) =>
             attachmentsComponent.components.length < 5
@@ -50,6 +74,8 @@ export default async function updateStarredMessage(
                  )
                : undefined,
          );
+
+         // Edit message
          const reaction = auxdibot.emojis.cache.get(starLevel.message_reaction) ?? starLevel.message_reaction;
          await message.edit({
             content: `**${count} ${reaction ?? ''}** | ${starredMessage.channel}`,
@@ -73,5 +99,7 @@ export default async function updateStarredMessage(
       } catch (x) {
          console.error(x);
       }
+   } else {
+      await deleteStarredMessage(auxdibot, guild, starredData).catch(() => undefined);
    }
 }
