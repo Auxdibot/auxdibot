2576ae4770d0c25791c90123ddd0718d540d4fdd
Steven <58159084+Auxdible@users.noreply.github.com>
2024-10-21 17:04:35 -0400

    fix(role): role deletion causes crash

--- src/interaction/events/roles/roleDelete.ts
+++ src/interaction/events/roles/roleDelete.ts
@@ -4,23 +4,27 @@ import findOrCreateServer from '@/modules/server/findOrCreateServer';
 export default async function roleDelete(auxdibot: Auxdibot, role: Role) {
    const server = await findOrCreateServer(auxdibot, role.guild.id);
 
-   await auxdibot.database.servers.update({
-      where: { serverID: role.guild.id },
-      data: {
-         level_rewards: server.level_rewards.filter((i) => i.roleID != role.id),
-         automod_role_exceptions: server.automod_role_exceptions.filter((i) => i != role.id),
-         join_roles: server.join_roles.filter((i) => i != role.id),
-         sticky_roles: server.sticky_roles.filter((i) => i != role.id),
-         report_role: server.report_role == role.id ? null : undefined,
-         mute_role: server.mute_role == role.id ? null : undefined,
-         role_multipliers: server.role_multipliers.filter((i) => i.id != role.id),
-         command_permissions: server.command_permissions.map((i) => ({
-            ...i,
-            blacklist_roles: i.blacklist_roles.filter((j) => j != role.id),
-            roles: i.roles.filter((j) => j != role.id),
-            permission_bypass_roles: i.permission_bypass_roles.filter((j) => j != role.id),
-         })),
-      },
-   });
+   await auxdibot.database.servers
+      .update({
+         where: { serverID: role.guild.id },
+         data: {
+            level_rewards: server.level_rewards.filter((i) => i.roleID != role.id),
+            automod_role_exceptions: server.automod_role_exceptions.filter((i) => i != role.id),
+            join_roles: server.join_roles.filter((i) => i != role.id),
+            sticky_roles: server.sticky_roles.filter((i) => i != role.id),
+            report_role: server.report_role == role.id ? null : undefined,
+            mute_role: server.mute_role == role.id ? null : undefined,
+            role_multipliers: server.role_multipliers.filter((i) => i.id != role.id),
+            command_permissions: server.command_permissions.map((i) => ({
+               ...i,
+               blacklist_roles: i.blacklist_roles.filter((j) => j != role.id),
+               roles: i.roles.filter((j) => j != role.id),
+               permission_bypass_roles: i.permission_bypass_roles.filter((j) => j != role.id),
+            })),
+         },
+      })
+      .catch((x) => {
+         console.error(x);
+      });
    return;
 }
