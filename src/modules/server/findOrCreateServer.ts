60c35ba7f623a3da4a65ff5e4162d58aaad0b6e1
Auxdible <stevenhotrod06@gmail.com>
2024-12-26 10:59:51 -0500

    fix: set hard limit for premium leaving uncapped causes crashes

--- src/modules/server/findOrCreateServer.ts
+++ src/modules/server/findOrCreateServer.ts
@@ -2,12 +2,9 @@ import { defaultServer } from '@/constants/database/defaultServer';
 import { servers } from '@prisma/client';
 import { Auxdibot } from '@/Auxdibot';
 export default async function findOrCreateServer(auxdibot: Auxdibot, serverID: string): Promise<servers | undefined> {
-   const current = Date.now();
-
    return await auxdibot.database.servers
       .upsert({ where: { serverID }, update: {}, create: { serverID, ...defaultServer } })
       .then((server) => {
-         console.log('FETCH SERVER ' + serverID + ' ' + current + ' ' + (Date.now() - current));
          return server;
       })
       .catch(() => undefined);
